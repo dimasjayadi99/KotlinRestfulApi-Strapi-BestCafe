e23c7bea8de9a7e667c867e2bf155db425fb6f2a
= <=>
2023-03-08 23:15:53 +0700

    Update gets data without refresh page on ProfileFragment

--- app/src/main/java/com/example/bestcafe/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/bestcafe/Fragment/ProfileFragment.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,8 +14,14 @@ import android.widget.Toast
 import androidx.cardview.widget.CardView
 import com.example.bestcafe.Activity.EditProfileActivity
 import com.example.bestcafe.Activity.LoginActivity
+import com.example.bestcafe.Api.User.ApiConfigUser
 import com.example.bestcafe.Api.User.SharedPrefManager
+import com.example.bestcafe.Api.User.UserModel
 import com.example.bestcafe.R
+import com.example.bestcafe.Response.ResponseUser
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileFragment : Fragment() {
 
@@ -25,19 +32,6 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = LayoutInflater.from(parentFragment?.context).inflate(R.layout.fragment_profile, container, false)
 
-        tv_username = view.findViewById(R.id.tv_username)
-        tv_email = view.findViewById(R.id.tv_email)
-        tv_edit = view.findViewById(R.id.tv_edit)
-        tv_edit.setOnClickListener{
-            val intent = Intent(it.context,EditProfileActivity::class.java)
-            startActivity(intent)
-        }
-
-        val dataUser = SharedPrefManager.getInstance(view.context).user
-
-        tv_username.text = dataUser.username
-        tv_email.text = dataUser.email
-
         val cd_logout = view.findViewById<CardView>(R.id.cd_logout)
         cd_logout.setOnClickListener{
             //logout action
@@ -45,6 +39,31 @@ class ProfileFragment : Fragment() {
         }
 
         return view
+    }
+
+    private fun getDataUser(id: Int) {
+        ApiConfigUser.getService().getDataUser(id).enqueue(object : Callback<List<UserModel>>{
+            override fun onResponse(
+                call: Call<List<UserModel>>,
+                response: Response<List<UserModel>>
+            ) {
+                if (response.isSuccessful){
+                    if (response.body() != null){
+                        val responseBody = response.body()
+                        val email = responseBody?.get(0)?.email
+                        val username = responseBody?.get(0)?.username
+
+                        tv_username.text = username
+                        tv_email.text = email
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<UserModel>>, t: Throwable) {
+
+            }
+
+        })
     }
 
     private fun logoutAccount(view: View?) {
@@ -65,6 +84,23 @@ class ProfileFragment : Fragment() {
             }
             alertDialog.create().show()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        tv_username = requireView().findViewById(R.id.tv_username)
+        tv_email = requireView().findViewById(R.id.tv_email)
+        tv_edit = requireView().findViewById(R.id.tv_edit)
+        tv_edit.setOnClickListener{
+            val intent = Intent(it.context,EditProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        val dataUser = SharedPrefManager.getInstance(requireView().context).user
+
+        val id = dataUser.id
+
+        getDataUser(id)
     }
 
 }
